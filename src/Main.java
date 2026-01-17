ecfb96ec7edb726a688d0421f6f94c5bc654da2b
Azamat Begimbayev <azamatbegimbaev50@gmail.com>
2026-01-17 09:42:41 +0500

    demo1

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+//demo
 public class Main {
     public static void main(String[] args) {
         System.out.println("Connecting to Supabase...");
