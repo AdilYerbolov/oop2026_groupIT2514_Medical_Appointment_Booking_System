acd86521cc5cd9ce5aaa4304a8a01def1890c8a0
AdilYerbolov <250610@astanait.edu.kz>
2026-01-24 23:21:15 +0500

    more minor bug fixes

--- src/repositories/appointmentDatabase.java
+++ src/repositories/appointmentDatabase.java
@@ -139,7 +139,7 @@ public class appointmentDatabase {
     }
     public static void viewUpcomingAppointmentsD(int doctorId, LocalDate today, LocalTime now){
         try (Connection connection = DatabaseConnection.getConnection()){
-            String sql = "select * from appointments where doctor_id = ? and appointment_date > ? and status = 'booked'";
+            String sql = "select * from appointments where doctor_id = ? and appointment_date > ? and status = 'Booked'";
             ResultSet rs = null;
             try (PreparedStatement stmt = connection.prepareStatement(sql)){
                 stmt.setInt(1, doctorId);
